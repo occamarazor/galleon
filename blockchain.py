64da86de97b9597d62ce2690ab6227948698f042
Pixel Warlock <30173929+occamarazor@users.noreply.github.com>
2022-06-01 14:46:08 +0300

    Blockchain: get_prev_block, proof_of_work

--- blockchain.py
+++ blockchain.py
@@ -1,16 +1,17 @@
-import hashlib
 import json
-from typing import TypedDict
+from hashlib import sha256
+from typing import TypedDict, Final
 from datetime import datetime
-
 from flask import Flask, jsonify
+
+TARGET_ZEROS: Final = '0000'
 
 
 # Block dict
 class Block(TypedDict):
     index: int
     timestamp: str
-    proof: int
+    nonce: int
     prev_hash: str
 
 
@@ -20,20 +21,41 @@ class Blockchain:
         # Create init hain
         self.chain = []
         # Create genesis block
-        self.create_block(proof=1, prev_hash='0')
+        self.create_block(block_nonce=1, block_prev_hash='0')
 
-    def create_block(self, proof: int, prev_hash: str) -> dict:
+    def create_block(self, block_nonce: int, block_prev_hash: str) -> dict:
         """Creates new block with data & appends it to the chain
-        :param proof: block proof
-        :param prev_hash: previous block hash
+        :param block_nonce: block nonce
+        :param block_prev_hash: previous block hash
         :return: new created block
         """
-        block: Block = {'index': len(self.chain) + 1,
-                        'timestamp': f'{datetime.now()}',
-                        'proof': proof,
-                        'prev_hash': prev_hash
-                        }
-        self.chain.append(block)
-        return block
+        new_block: Block = {'index': len(self.chain) + 1,
+                            'timestamp': f'{datetime.now()}',
+                            'nonce': block_nonce,
+                            'prev_hash': block_prev_hash
+                            }
+        self.chain.append(new_block)
+        return new_block
+
+    def get_prev_block(self):
+        return self.chain[-1]
+
+    def proof_of_work(self, prev_block_nonce: int) -> int:
+        """ Solves the cryptographic puzzle
+        :param prev_block_nonce: previous block nonce
+        :return: new block nonce
+        """
+        new_block_nonce: int = 1
+        nonce_is_valid: bool = False
+
+        while nonce_is_valid is False:
+            possible_hash: str = sha256(f'{new_block_nonce ** 2 - prev_block_nonce ** 2}'.encode()).hexdigest()
+
+            if possible_hash[:4] == TARGET_ZEROS:
+                nonce_is_valid = True
+            else:
+                new_block_nonce += 1
+
+        return new_block_nonce
 
 # Mining
