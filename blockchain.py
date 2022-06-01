56583550b79f277eb4f204138e1127a3c92a8619
Pixel Warlock <30173929+occamarazor@users.noreply.github.com>
2022-06-01 17:11:09 +0300

    Blockchain: mine_new_block route

--- blockchain.py
+++ blockchain.py
@@ -4,7 +4,10 @@ from typing import TypedDict, Final
 from datetime import datetime
 from flask import Flask, jsonify
 
+# 1) Build blockchain
+
 TARGET_ZEROS: Final = '0000'
+
 
 # Block annotation
 class Block(TypedDict):
@@ -20,21 +23,26 @@ class Blockchain:
         # Create init hain
         self.chain: list[Block] = []
         # Create genesis block
-        self.create_block(block_nonce=1, block_prev_hash='0')
+        self.create_block(new_block_nonce=1, prev_block_hash='0')
 
     def calc_block_hash(self, block_nonce: int, prev_block_nonce: int) -> str:
+        """ Computes block hash solving the cryptographic puzzle
+        :param block_nonce: block nonce
+        :param prev_block_nonce: previous block nonce
+        :return: block hash solving the cryptographic puzzle
+        """
         return sha256(f'{block_nonce ** 2 - prev_block_nonce ** 2}'.encode()).hexdigest()
 
-    def create_block(self, block_nonce: int, block_prev_hash: str) -> Block:
+    def create_block(self, new_block_nonce: int, prev_block_hash: str) -> Block:
         """Creates a new block with data & appends it to the chain
-        :param block_nonce: block nonce
-        :param block_prev_hash: previous block hash
+        :param new_block_nonce: new block nonce
+        :param prev_block_hash: previous block hash
         :return: new created block
         """
         new_block: Block = {'index': len(self.chain) + 1,
                             'timestamp': f'{datetime.now()}',
-                            'nonce': block_nonce,
-                            'prev_hash': block_prev_hash
+                            'nonce': new_block_nonce,
+                            'prev_hash': prev_block_hash,
                             }
         self.chain.append(new_block)
         return new_block
@@ -64,7 +72,7 @@ class Blockchain:
     def hash_block(self, block: Block) -> str:
         """ Computes entire block hash
         :param block: block
-        :return: encoded block hash
+        :return: entire block hash
         """
         encoded_block = json.dumps(block, sort_keys=True).encode()
         return sha256(encoded_block).hexdigest()
@@ -96,4 +104,22 @@ class Blockchain:
             block_index += 1
             return True
 
-# Mining
+
+# 2) Create webapp & blockchain
+app = Flask(__name__)
+blockchain = Blockchain()
+
+
+# 3) Mine a new block
+@app.route('/mine_new_block', methods=['GET'])
+def mine_new_block():
+    prev_block = blockchain.get_prev_block()
+    prev_block_nonce = prev_block['nonce']
+    new_block_nonce = blockchain.proof_of_work(prev_block_nonce)
+    prev_block_hash = blockchain.hash_block(prev_block)
+    new_block = blockchain.create_block(new_block_nonce, prev_block_hash)
+
+    response = {key: val for key, val in new_block}
+    response['message'] = 'Yay, you just mined a block!'
+
+    return jsonify(response), 200
